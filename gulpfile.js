d56dd714e24ff3d31f8b3983487192c7d6e931f8
wolfather <so.israelweb@gmail.com>
2017-02-09 22:13:42 -0200

    Refactoring structures and gulp to minify the javascript file

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,20 @@
 const gulp 	= require('gulp'),
 	sass 	= require('gulp-sass'),
 	concat 	= require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename')
 	svgsprite = require('gulp-svg-sprite'),
 	sourcemaps = require('gulp-sourcemaps'),
 	jade = require('gulp-jade'),
 	watch = require('gulp-watch')
 
-
 gulp
 	.task('watch', ()=> {
 		gulp.watch('./dev/**/*.scss', ['sass']);
 		gulp.watch('./dev/**/*.jade', ['jade']);
-		gulp.watch('./dev/**/*.js', ['concat']);
+		gulp.watch('./dev/**/*.js', ['uglify']);
 	})
-	.task('default', ['svgsprite', 'sass', 'jade', 'concat'])
+	.task('default', ['svgsprite', 'sass', 'jade', 'uglify'])
 	.task('sass', ()=> {
 		return gulp.src('./dev/main.scss')
 			.pipe(sourcemaps.init())
@@ -45,24 +46,30 @@ gulp
 				}))
 				.pipe(gulp.dest('./public/stylesheet'))
 	})
-	.task('jade', function() {
-		var YOUR_LOCALS = {};
-
+	.task('jade', ()=> {
 		gulp.src('./dev/**/*.jade')
 		.pipe(jade())
 		.pipe(gulp.dest('./public/template'))
 	})
-	.task('concat', ()=> {
-		return gulp.src([
-				'./bower_components/angular/angular.min.js',
-				'./dev/components/**/*.js',
-				'./dev/cardprofilecomponent/**/*.js',
-				'./dev/listcomponent/**/*.js',
-				'./dev/skillcomponent/**/*.js',
-				'./dev/titlecomponent/**/*.js',
-				'./dev/viewprofilecomponent/**/*.js',
-				'./dev/main.js'
-			])
-			.pipe(concat('app.js'))
-			.pipe(gulp.dest('./public/javascript'))
+	.task('uglify', ()=> {
+		gulp.src([
+			'./bower_components/angular/angular.min.js',
+			'./dev/components/**/*.js',
+			'./dev/cardprofilecomponent/**/*.js',
+			'./dev/listcomponent/**/*.js',
+			'./dev/skillcomponent/**/*.js',
+			'./dev/titlecomponent/**/*.js',
+			'./dev/viewprofilecomponent/**/*.js',
+			'./dev/main.js'
+		])
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./dev'))
+		.pipe(rename('app.min.js'))
+		.pipe(uglify({
+				compress: true,
+				mangle: false,
+				preserveComponents: true,
+				licence: true
+		}))
+		.pipe(gulp.dest('./public/javascript'))
 	})
